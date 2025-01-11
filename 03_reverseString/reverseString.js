27b51e6efa60b380d403fc424fe82f5e146c9060
Juancar18fp <38106370+Juancar18fp@users.noreply.github.com>
2025-01-11 20:05:47 +0100

    Complete exercise 03 of java-script-exercises
    Complete exercise 03 of java-script-exercise
    
    Implemented the function reverseString that takes a string as a parameter
    and returns the same word but reversed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split("");
+    let reverseWordArray = wordArray.reverse();
+    return reverseWordArray.join("");
 };
 
 // Do not edit below this line
