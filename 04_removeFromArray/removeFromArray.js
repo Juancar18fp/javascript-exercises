a978fb695ea51f968016c85e2121fc89e021067d
Juancar18fp <38106370+Juancar18fp@users.noreply.github.com>
2025-01-12 17:52:45 +0100

    Complete exercie 04 of java-script-exercises
    Made a method that takes an array and an indefinite number
    of numbers as parameters. If any of these numbers are present in the
    array, they are removed. It returns the array without the numbers
    passed as arguments.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayOfNumbers, ...args) {
+    let newArray = []
+    arrayOfNumbers.forEach(element => {
+        if(!args.includes(element)){
+            newArray.push(element);
+        }
+    });
+    return newArray;
+    // return arrayOfNumbers.filter(element => !args.include(element));
 };
 
 // Do not edit below this line
