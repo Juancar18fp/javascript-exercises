07e276cdee69c02b93cf69619a1e552a73c36fc4
Juancar18fp <38106370+Juancar18fp@users.noreply.github.com>
2025-01-12 18:16:18 +0100

    Complete exercise 04 of java-script-exercises
    Made a method that takes tow integers as parameters and returns the sum
    of every integer between. If are not integers or negatives returns ERROR.
    The first number can be the big one and the sum is calculated by gauss
    formula.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (first, last) {
+    if (first < 0 || last < 0 || !Number.isInteger(first) || !Number.isInteger(last)) return "ERROR";
+    if (first > last) {
+        let temp = first;
+        first = last;
+        last = temp;
+    }
+    return (last * (last + 1) / 2) - (first * (first - 1) / 2);
 };
 
 // Do not edit below this line
